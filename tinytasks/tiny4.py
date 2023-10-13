9a88ba0d132c8ecf27e035db0c72c85c7d66791f
hitogava <hitogavax@yandex.ru>
2023-10-14 00:59:57 +0700

    Clean tiny4.py

--- tinytasks/tiny4.py
+++ tinytasks/tiny4.py
@@ -1,8 +1,4 @@
-# d = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 3}
-d = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}
-# d = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97835}
 new_d = {}
 for value in set(d.values()):
     keys = tuple([k for k in d.keys() if d[k] == value])
     new_d[value] = keys if len(keys) > 1 else keys[0]
-print(new_d)
