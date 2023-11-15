7e29bc58559b19576416db1f4560f13936303fb6
hitogava <hitogavax@yandex.ru>
2023-11-15 22:44:40 +0700

    Task 11

--- tinytasks/tiny11.py
+++ tinytasks/tiny11.py
@@ -7,16 +7,17 @@ class A:
 class Singleton:
     instance = None
     def __new__(cls, *args, **kwargs):
-        if Singleton.instance is None:
-            Singleton.instance = super().__new__(cls)
-            super().__init__(cls, *args[0])
-        return Singleton.instance
-    def __init__(self):
-        pass
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+            cls.__init__(cls, *args[0])
+            cls.__init__ = lambda *args: None
+        return cls.instance
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 
 class GlobalA(Singleton, A):
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls, args, kwargs)
-    def __init__(self, a, b):
-        super().__init__()
