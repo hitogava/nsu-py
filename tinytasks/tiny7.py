7d572708a490437c0d58bd9f88fe696b868a912b
hitogava <hitogavax@yandex.ru>
2023-12-04 19:09:17 +0700

    Tiny7

--- tinytasks/tiny7.py
+++ tinytasks/tiny7.py
@@ -1,15 +1,17 @@
-def get_max_fold_level(l, current_level):
-    if not isinstance(l,list): return current_level - 1
-    return max(get_max_fold_level(x, current_level+1) for x in l)
-
-
 def flatten(currentlist, depth=None):
-    if depth is None:
-        depth = get_max_fold_level(currentlist, 0)
     res = []
     for el in currentlist:
-        if isinstance(el, list) and depth:
-            res.extend(flatten(el, depth-1))
+        if isinstance(el, list):
+            if depth is None:
+                res.extend(flatten(el))
+            elif depth:
+                res.extend(flatten(el,depth-1))
+            else:
+                res.append(el)
         else:
             res.append(el)
     return res
+
+
+print(flatten([1, 2, [4, 5], [6, [7]], 8], depth=0))
+print(flatten([1, 2, [4, 5], [6, [7]], 8]))
